b6ccd5542684930263970f62086d727cff6d0a06
Lance Liu <lanceliumeng89@gmail.com>
2022-06-01 18:21:13 +1000

    add Paper,Container and Grid from MUI5

---
+++ src/components/TourCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper, Grid } from "@mui/material";
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={6}>Hey~</Paper>
+    </Grid>
+  );
+};
+
+export default TourCard;
