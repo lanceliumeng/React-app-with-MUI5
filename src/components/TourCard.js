3989844589524862e108702ef2bed5acb87f8cfe
Lance Liu <lanceliumeng89@gmail.com>
2022-06-01 22:52:05 +1000

    use MUI AppBar component to create navbar

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,41 +1,68 @@
 import React from "react";
 import { Paper, Grid, Typography, Box, Rating } from "@mui/material";
 import AccessTime from "@mui/icons-material/AccessTime";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={6}>
-        <img
-          src="https://images.unsplash.com/photo-1618032593076-64793c24427d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"
-          alt=""
-          className="img"
-        ></img>
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Bay of Fire
-          </Typography>
-
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ width: 14.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={6}>
+          <img
+            src="https://images.unsplash.com/photo-1618032593076-64793c24427d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"
+            alt=""
+            className="img"
+          ></img>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Bay of Fire
             </Typography>
-          </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: 3,
-            }}
-          >
-            {/* <Rating
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 14.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: 3,
+              }}
+            >
+              {/* <Rating
               sx={{ color: "#ff6d75" }}
               name="read-only"
               value={4.5}
@@ -46,28 +73,29 @@ const TourCard = () => {
               size="small"
             /> */}
 
-            <Rating
-              name="read-only"
-              value={4.5}
-              precision={0.5}
-              readOnly
-              size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
+              <Rating
+                name="read-only"
+                value={4.5}
+                precision={0.5}
+                readOnly
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
 
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0.3}>
-              From $600
-            </Typography>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0.3}>
+                From $600
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
